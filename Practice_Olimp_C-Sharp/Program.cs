2dd67cb2dc46734cd9bd90b4edc0514498cbfdbd
Vasyl <vzdendyak@gmail.com>
2020-05-08 17:58:42 +0300

    Add project files.

---
+++ Practice_Olimp_C-Sharp/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice_Olimp_C_Sharp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //perebirMnog(4);
+            //perebirMnogRecurs(1, 4);
+            int[] arr = { 1, 2, 3, 4 };
+
+            //perestanovka(arr, 4);
+
+            Console.WriteLine("Finish!");
+            Console.ReadLine();
+        }
+
+        #region Перебір всіх підможин без повторень (звич + рекурсія)
+
+        // Перебір всіх підможин без повторень
+        private static void perebirMnog(int k)
+        {
+            string allEl = "";
+            for (int i = 1; i <= k; i++)
+            {
+                Console.WriteLine($"{{{i}}}");
+                allEl += $"{i},";
+                for (int j = i + 1; j <= k; j++)
+                {
+                    Console.WriteLine($"{{{i},{j}}}");
+                }
+            }
+            Console.WriteLine($"{{{allEl}}}");
+        }
+
+        //Перебір всіх підможин без повторень - рекурсія
+        private static void perebirMnogRecurs(int n, int k)
+        {
+            if (n > k)
+            {
+                Console.Write("{");
+                for (int i = 1; i <= k; i++)
+                {
+                    Console.Write(i + ",");
+                }
+                Console.WriteLine("}");
+            }
+            else
+            {
+                Console.WriteLine($"{{{n}}}");
+                for (int i = n + 1; i <= k; i++)
+                {
+                    Console.WriteLine($"{{{n},{i}}}");
+                }
+                perebirMnogRecurs(n + 1, k);
+            }
+        }
+
+        #endregion Перебір всіх підможин без повторень (звич + рекурсія)
+
+        #region Перебір усіх перестановок з n елементів
+
+        // Перебір усіх перестановок з n елементів
+        private static void perestanovka(int[] arr, int n)
+        {
+            bool state = false;
+            while (!state)
+            {
+                for (int i = arr.Length - 1; i > 0; i--)
+                {
+                    if (arr[i - 1] < arr[i])
+                    {
+                        Print(arr);
+                        swap(arr, i - 1, find_min_arr(arr, i));
+                        arr_sort(arr, i);
+                        state = false;
+                        break;
+                    }
+                    else
+                    {
+                        state = true;
+                    }
+                }
+            }
+            Print(arr); return;
+        }
+
+        private static void arr_sort(int[] arr, int n)
+        {
+            for (int i = n; i < arr.Length - 1; i++)
+            {
+                for (int j = n; j < arr.Length - i - 1 + n; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // меняем элементы местами
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        private static int find_min_arr(int[] arr, int n)
+        {
+            int max = arr[n];
+            int maxIndex = n;
+            for (int i = n; i < arr.Length; i++)
+            {
+                if (arr[i] < max && arr[i] > arr[n - 1])
+                {
+                    max = arr[i];
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+
+        private static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+        }
+
+        private static void swap(int[] a, int i, int j)
+        {
+            int s = a[i];
+            a[i] = a[j];
+            a[j] = s;
+        }
+
+        #endregion Перебір усіх перестановок з n елементів
+    }
+}
