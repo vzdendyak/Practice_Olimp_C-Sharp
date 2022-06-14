0ab18c083a9a011afd68c588b54f79592c6f386f
vzdendyak <User@USER-PC>
2022-06-14 18:05:52 +0300

    i dont know. revert that

--- AOS_University/Lab5_Calculator.cs
+++ AOS_University/Lab5_Calculator.cs
@@ -5,90 +5,32 @@ namespace AOS_University
 {
     public class Lab5_Calculator
     {
-        private const double t_obs = 0.1;
-        private const double lambda_def = 520;
-        public const int n = 53;
-        private const double Pwx = 0.829;
-        private const double Nx = 0.58;
-        private double Pw = 0.307;
+        private const double t_obs = 0.4;
+        private const double lambda_def = 148.3;
+        public const int n = 68;
+        private double Pw = 0.19;
 
-        //public const double t_obs = 0.2;
-        //public const double lambda = 280;
-        //public const int n = 58;
-
-        // n opt = 58!!!!
         private double u = 1 / t_obs;
 
         public void Calculate()
         {
             Console.WriteLine("Po = " + Get_P_0());
             Console.WriteLine("Pw = " + Get_P_w());
-            //var dictionary = Calculate_1_N();
-            //var dictionary2 = Calculate_2_Lambda();
-            //var dictionary3 = Calculate_3_t_obs();
+            var dictionary2 = Calculate_2_Lambda();
             Console.WriteLine();
-        }
-
-        public Dictionary<int, double> Calculate_1_N()
-        {
-            var calc_n = n;
-            double Pw = Get_P_w(n: calc_n);
-            double Pw_increase = Get_P_w(n: calc_n + 1);
-            double Pw_reduce = Get_P_w(n: calc_n - 1);
-            var values_dictionary = new Dictionary<int, double>();
-            calc_n = n;
-            if (Pw_increase < Pw)
-            {
-                log("Increase better!", ConsoleColor.Green);
-                for (int i = 0; i < 20; i++, calc_n++)
-                {
-                    var value = Get_P_w(n: calc_n);
-                    if (calc_n == n || Math.Round(value, 3) == this.Pw)
-                    {
-                        log($"N = {calc_n}\nPw={value}", ConsoleColor.Red);
-                    }
-                    else
-                    {
-                        log($"N = {calc_n}\nPw={value}", ConsoleColor.Cyan);
-                    }
-                    values_dictionary.Add(calc_n, value);
-                }
-            }
-            else if (Pw_reduce < Pw)
-            {
-                log("Reduce better!", ConsoleColor.Red);
-                for (int i = 0; i < 20; i++, calc_n--)
-                {
-                    var value = Get_P_w(n: calc_n);
-                    if (calc_n == n || Math.Round(value, 3) == this.Pw)
-                    {
-                        log($"N = {calc_n}\nPw={value}", ConsoleColor.Red);
-                    }
-                    else
-                    {
-                        log($"N = {calc_n}\nPw={value}", ConsoleColor.Blue);
-                    }
-                    values_dictionary.Add(calc_n, value);
-                }
-            }
-            else
-            {
-                log("Nothing...", ConsoleColor.Yellow);
-            }
-            return values_dictionary;
         }
 
         public Dictionary<double, double> Calculate_2_Lambda()
         {
             var calc_lambda = lambda_def;
-            double reduceStep = 0.5;
+            double reduceStep = 0.1;
             double Pw = Get_P_w(lambda: calc_lambda);
             double Pw_increase = Get_P_w(lambda: calc_lambda + reduceStep);
             double Pw_reduce = Get_P_w(lambda: calc_lambda - reduceStep);
             var values_dictionary = new Dictionary<double, double>();
-            int steps = 40;
+            int steps = 500;
             calc_lambda = lambda_def;
-            if (Pw_increase < Pw)
+            if (true)
             {
                 log("Increase better!", ConsoleColor.Green);
                 for (int i = 0; i < steps; i++, calc_lambda += reduceStep)
@@ -96,17 +38,18 @@ namespace AOS_University
                     var value = Get_P_w(lambda: calc_lambda);
                     if (calc_lambda == lambda_def || Math.Round(value, 3) == this.Pw)
                     {
-                        log($"lambda = {calc_lambda}\nPw={value}", ConsoleColor.Red);
+                        log($"lambda = {Math.Round(calc_lambda, 3)}\nPw={Math.Round(value, 3)}", ConsoleColor.Red);
                         Console.Beep();
                     }
                     else
                     {
-                        log($"lambda = {calc_lambda}\nPw={value}", ConsoleColor.Cyan);
+                        log($"lambda = {Math.Round(calc_lambda, 3)}\nPw={Math.Round(value, 3)}", ConsoleColor.Cyan);
                     }
+
                     values_dictionary.Add(calc_lambda, value);
                 }
             }
-            else if (Pw_reduce < Pw)
+            else if (Pw_reduce < Pw && Pw_reduce > 0)
             {
                 log("Reduce better!", ConsoleColor.Red);
                 for (int i = 0; i < steps; i++, calc_lambda -= reduceStep)
@@ -130,55 +73,6 @@ namespace AOS_University
             return values_dictionary;
         }
 
-        public Dictionary<double, double> Calculate_3_t_obs()
-        {
-            var calc_t_obs = t_obs;
-            double Pw = Get_P_w(t_obs: calc_t_obs);
-            double Pw_increase = Get_P_w(t_obs: calc_t_obs + 0.1);
-            double Pw_reduce = Get_P_w(t_obs: calc_t_obs - 0.1);
-            var values_dictionary = new Dictionary<double, double>();
-            int steps = 20;
-            calc_t_obs = t_obs;
-            if (Pw_increase < Pw)
-            {
-                log("Increase better!", ConsoleColor.Green);
-                for (int i = 0; i < steps; i++, calc_t_obs += 0.01)
-                {
-                    var value = Get_P_w(t_obs: calc_t_obs);
-                    if (calc_t_obs == t_obs || Math.Round(value, 3) == this.Pw)
-                    {
-                        log($"t_obs = {calc_t_obs}\nPw={value}", ConsoleColor.Red);
-                    }
-                    else
-                    {
-                        log($"t_obs = {calc_t_obs}\nPw={value}", ConsoleColor.Cyan);
-                    }
-                    values_dictionary.Add(calc_t_obs, value);
-                }
-            }
-            else if (Pw_reduce < Pw)
-            {
-                log("Reduce better!", ConsoleColor.Red);
-                for (int i = 0; i < steps; i++, calc_t_obs -= 0.01)
-                {
-                    var value = Get_P_w(t_obs: calc_t_obs);
-                    if (calc_t_obs == t_obs)
-                    {
-                        log($"t_obs = {calc_t_obs}\nPw={value}", ConsoleColor.Red);
-                    }
-                    else
-                    {
-                        log($"t_obs = {calc_t_obs}\nPw={value}", ConsoleColor.Blue);
-                    }
-                    values_dictionary.Add(calc_t_obs, value);
-                }
-            }
-            else
-                log("Nothing...", ConsoleColor.Yellow);
-
-            return values_dictionary;
-        }
-
         public double Get_P_0(int n = n, double t_obs = t_obs, double lambda = lambda_def)
         {
             u = 1 / t_obs;
@@ -194,6 +88,10 @@ namespace AOS_University
 
         public double Get_P_w(int n = n, double t_obs = t_obs, double lambda = lambda_def)
         {
+            if (lambda == 490)
+            {
+                Console.WriteLine("test");
+            }
             u = 1 / t_obs;
             double top_part = u * Get_P_0(n: n, lambda: lambda); // u * Po
             double first_drib = top_part / Get_n_fact_minus(n: n, lambda: lambda);
